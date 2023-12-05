e35bf1b5dbd5b52c2e208da7bb894cb38caa87cc
retroroo <152475238+retroroo@users.noreply.github.com>
2023-12-05 00:03:16 -0800

    menu working
    3 am will do more tomorrow

--- Assets/Scripts/MenuSelect.cs
+++ Assets/Scripts/MenuSelect.cs
@@ -1,6 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MenuSelect : MonoBehaviour
 {
@@ -9,6 +14,8 @@ public class MenuSelect : MonoBehaviour
     public GameObject[] objectsToHide; // Array of objects to hide for each number
     public int maxNumber = 10;
     public int currentNumber = 0;
+
+	public GameObject warning;
 
     void Update()
     {
@@ -40,13 +47,62 @@ public class MenuSelect : MonoBehaviour
                 objectsToHide[i].SetActive(i != currentNumber);
             }
         }
-
-        // Check for Enter press
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (use3DSControls){
+				if (UnityEngine.N3DS.GamePad.GetButtonHold(N3dsButton.A))
+				{
+					Debug.Log("Number " + currentNumber + " is selected");
+					menuLogic();
+				}
+		}else{
+			// Check for Enter press
+			if (Input.GetKeyDown(KeyCode.Return))
+			{
+				Debug.Log("Number " + currentNumber + " is selected");
+				menuLogic();
+				// Custom code can be added here
+			}
+		}      
+    }
+	private void menuLogic(){
+		if(currentNumber == 4){
+				QuitGame();
+			}else if(currentNumber == 1)
+			{
+				LoadScene("Tutorial");
+			}
+			else{
+				StartCoroutine(ActivateAndDeactivate(warning, 3));
+			}
+	}
+	 private IEnumerator ActivateAndDeactivate(GameObject obj, float seconds)
+    {
+        if (obj != null)
         {
-            Debug.Log("Number " + currentNumber + " is selected");
-
-            // Custom code can be added here
+            obj.SetActive(true);
+            yield return new WaitForSeconds(seconds);
+            obj.SetActive(false);
+        }
+    }
+	public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        // This code will only execute in the Unity Editor
+        EditorApplication.isPlaying = false;
+        #else
+        // This code will only execute in the build version
+        Application.Quit();
+        #endif
+    }
+	public void LoadScene(string sceneToLoad)
+    {
+        // Make sure the scene name is not empty
+        if (!string.IsNullOrEmpty(sceneToLoad))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            Debug.LogError("Scene name is empty!");
         }
     }
 }
---
+++ Assets/Scripts/SceneTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTrigger : MonoBehaviour {
+	// Name of the scene you want to load
+    public string sceneToLoad;
+
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		LoadScene();
+	}
+
+    // This method can be called to load the scene
+    public void LoadScene()
+    {
+        // Make sure the scene name is not empty
+        if (!string.IsNullOrEmpty(sceneToLoad))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            Debug.LogError("Scene name is empty!");
+        }
+    }
+}
